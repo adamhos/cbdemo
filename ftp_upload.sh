5647b81f9a7289e66f6a838e68d81a5201d062ea
Adam Hostetler <ahostetler@microsolved.com>
2019-11-21 19:11:21 -0500

    initial commit

---
+++ ftp_upload.sh
@@ -0,0 +1,19 @@
+ftp_host="clawback.microsolved.com"
+user="ftpupload"
+pass="$ecr3tp4ss"
+path="/"
+
+do
+ftp -in <<EOF
+open $ftp_host
+user $user$pass
+cd $path
+put $1
+close 
+bye
+EOF
+echo$1 uploaded to $path 
+
+# Break, otherwise endless loop
+break
+done
---
+++ utility.py
@@ -0,0 +1,9 @@
+import os
+import sys
+from msi_internal_libs import do_thing
+
+if 1 == 1:
+    x = do_thing()
+
+print("Thing was done successfully")
+    
